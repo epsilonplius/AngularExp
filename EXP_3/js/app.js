3bd5b926250bce761b43ac9f7db45b1c4d7c7a74
epsilonplius <aleksandras@epsilonplius.com>
2015-03-29 02:01:53 +0200

    ng-repeat on gems

--- EXP_3/js/app.js
+++ EXP_3/js/app.js
@@ -1,17 +1,35 @@
 (function () {
     var app = angular.module('store', []);
 
-    var gem = {
+    var gems = [{
         name: 'Some Gem',
         price: 10,
         description: 'sjgfskjgfsjdgfsjdgfskj',
         canPurchase: true,
         soldOut: true
-    };
+    }, {
+        name: 'Some Gem 2',
+        price: 30,
+        description: 'aaaaaaaaaaaaaa',
+        canPurchase: true,
+        soldOut: false
+    }, {
+        name: 'Some Gem 3',
+        price: 30,
+        description: 'bbbbbbbbbbbbb',
+        canPurchase: false,
+        soldOut: false
+    }, {
+        name: 'Some Gem 4',
+        price: 60,
+        description: 'eeeeeeeeeee',
+        canPurchase: false,
+        soldOut: false
+    }];
 
 
     app.controller('StoreController', function () {
-        this.product = gem;
+        this.products = gems;
     });
 
 
