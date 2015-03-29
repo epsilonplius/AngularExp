4e9b927d55f8cc120ec170e6b55c75bf9f3ff6a5
epsilonplius <aleksandras@epsilonplius.com>
2015-03-29 20:48:49 +0300

    filters

--- EXP_2_1/js/app.js
+++ EXP_2_1/js/app.js
@@ -4,24 +4,28 @@
     var gems = [{
         name: 'Some Gem',
         price: 10,
+        publishDate: '1388123412323',
         description: 'sjgfskjgfsjdgfsjdgfskj',
         canPurchase: true,
         soldOut: true
     }, {
         name: 'Some Gem 2',
         price: 30,
+        publishDate: '1388123412323',
         description: 'aaaaaaaaaaaaaa',
         canPurchase: true,
         soldOut: false
     }, {
         name: 'Some Gem 3',
         price: 30,
+        publishDate: '1388123412323',
         description: 'bbbbbbbbbbbbb',
         canPurchase: false,
         soldOut: false
     }, {
         name: 'Some Gem 4',
         price: 60,
+        publishDate: '1388123412323',
         description: 'eeeeeeeeeee',
         canPurchase: false,
         soldOut: false
